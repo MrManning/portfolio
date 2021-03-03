322918fcce9cb149ee9a46b23eacc4547389b51f
Kyle Manning <52970594+MrManning@users.noreply.github.com>
2021-03-03 02:13:54 +0000

    Add social icons to footer

--- src/pages/sections/footer/FooterSection.js
+++ src/pages/sections/footer/FooterSection.js
@@ -2,10 +2,36 @@ import React from "react";
 import Section from "../../Section";
 
 function FooterSection() {
+  const year = new Date().getFullYear();
+
   return (
-    <Section title={"Get in contact"} styling={"bottom"}>
+    <Section title={""} styling={"bottom"}>
       <div className={"content"}>
-        <p>&copy; 2020 Kyle Manning. All rights reserved.</p>
+        <div className="text">
+          <h2>Contact Me</h2>
+          <ul>
+            <li>
+              <a
+                href={"https://www.linkedin.com/in/kylecwmanning/"}
+                rel={"noopener noreferrer"}
+              >
+                <img
+                  src={"/icons/linkedin-filled-square.png"}
+                  alt={"LinkedIn"}
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href={"https://github.com/MrManning"}
+                rel={"noopener noreferrer"}
+              >
+                <img src={"/icons/github-filled-square.png"} alt={"Github"} />
+              </a>
+            </li>
+          </ul>
+          <p>&copy;{year} Kyle Manning. All rights reserved.</p>
+        </div>
       </div>
     </Section>
   );
